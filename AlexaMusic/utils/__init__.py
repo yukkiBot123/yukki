d2a790c92077d27e0f89715d3c126642916539df
AloneX <116536390+AloneopBot@users.noreply.github.com>
2022-11-12 23:51:47 +0530

    Update __init__.py

--- AlexaMusic/utils/__init__.py
+++ AlexaMusic/utils/__init__.py
@@ -1,12 +1,12 @@
 #
-# Copyright (C) 2021-2022 by Alexa_Help@Github, < https://github.com/Jankarikiduniya >.
+# Copyright (C) 2021-2022 by The_Alone_Network@Github, < https://github.com/AloneopBot >.
 # A Powerful Music Bot Property Of Rocks Indian Largest Chatting Group
 
-# Kanged By © @Dr_Asad_Ali
-# Rocks © @Shayri_Music_Lovers
-# Owner Asad Ali
-# Harshit Sharma
-# All rights reserved. © Alisha © Alexa © Yukki
+# Kanged By © @ALONE_WAS_,BOT
+# Rocks © @The_Alone_Network
+# Owner ALONE
+# ALONE
+# All rights reserved. © Alone © Alexa © Yukki
 
 
 from .channelplay import *
